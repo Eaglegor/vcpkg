69f8224787c5b7b130cc891e27646de2103c4b69
Nikolay Gorbachenko <eaglegor@gmail.com>
2022-02-11 01:01:21 +0300

    Configurable but not buildable osrm-backend

--- ports/osrm-backend/portfile.cmake
+++ ports/osrm-backend/portfile.cmake
@@ -4,10 +4,15 @@ vcpkg_from_github(
     REF v5.26.0
     SHA512 77c697bd9e9458db38ae8636766b8631a2a103c1c46ed7818b51e3edb5708ff6e7ab71430157532d4cd1ddd34f61904b920880fc8d3b240f671637df5fd61c08
     HEAD_REF master
+    PATCHES remove-zlib-boost-component.patch
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        ${OPTIONS}
+        ${FEATURE_OPTIONS}
+        -DBUILD_SHARED_LIBS=OFF
 )
 
 vcpkg_cmake_install()
